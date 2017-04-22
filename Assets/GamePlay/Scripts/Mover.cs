e47903c1f51c9af56166da3ca2f0c862950e0d7c
Simão Menezes <simaomenezesneto@gmail.com>
2017-04-22 09:58:37 -0300

    Adicionando script de movimentacao aos veiculos, criando script de movimentacao veiculos etc.

---
+++ Assets/GamePlay/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+	}
+}
