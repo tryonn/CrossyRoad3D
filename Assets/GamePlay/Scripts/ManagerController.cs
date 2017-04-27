d26006a94d02d3849438fd57352fd51ae47ad9e1
Simão Menezes <simaomenezesneto@gmail.com>
2017-04-26 23:03:22 -0300

    Criando metodo para atualizar moedas e tomates, criando cronometro para tempo no game, criando um enum para estados do game.

--- Assets/GamePlay/Scripts/ManagerController.cs
+++ Assets/GamePlay/Scripts/ManagerController.cs
@@ -1,11 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState
+{
+    TITULO,
+    GAMEPLAY,
+    GAMEOVER,
+    FASECONCLUIDA
+}
 
 public class ManagerController : MonoBehaviour {
 
+    public GameState currentState;
+
+    [Header("HUD GAME")]
+    public GameObject hudInfoGamePlay, hudTitulo, hudGameOver, hudLoading, hudLoja;
+
+    [Header("HUD GAME Atributos")]
+    public Text moedasTxt, tempoTxt, faseTxt, moedasMapaTxt, tomateMapaTxt;
+
+    
+    public int moedas, tempo, fase, moedasMapa, tomatesMapa, moedasColetadas, tomatesColetados;
+
+
 	// Use this for initialization
 	void Start () {
+
+        switch (currentState)
+        {
+            case GameState.TITULO:
+                break;
+            case GameState.GAMEPLAY:
+                StartCoroutine("ContagemRegressiva");
+                break;
+        }
+
 		
 	}
 	
@@ -13,4 +44,37 @@ public class ManagerController : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    #region Metodos criados pelo desenvolvedor
+
+    IEnumerator ContagemRegressiva()
+    {
+        tempoTxt.text = tempo.ToString();
+        yield return new WaitForSeconds(1);
+        tempo -= 1;
+
+        if (tempo == 0)
+        {
+            currentState = GameState.GAMEOVER;
+        }
+
+        if (currentState == GameState.GAMEPLAY)
+        {
+            StartCoroutine("ContagemRegressiva");
+        }
+    }
+    public void AtualizarMoedas(int valor)
+    {
+        moedas += valor;
+        moedasTxt.text = moedas.ToString();
+        moedasColetadas += 1;
+    }
+
+    public void AtualizarTempo(int valor)
+    {
+        tempo += valor;
+        tempoTxt.text = tempo.ToString();
+        tomatesColetados += 1;
+    }
+    #endregion
 }
