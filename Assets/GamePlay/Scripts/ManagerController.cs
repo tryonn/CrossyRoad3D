558e26b318796305d94997800f1950196a4dee83
Simão Menezes <simaomenezesneto@gmail.com>
2017-04-29 10:55:36 -0300

    Implementando seleção de personagem

--- Assets/GamePlay/Scripts/ManagerController.cs
+++ Assets/GamePlay/Scripts/ManagerController.cs
@@ -28,9 +28,22 @@ public class ManagerController : MonoBehaviour {
     public int moedas, tempo, fase, moedasMapa, tomatesMapa, moedasColetadas, tomatesColetados, precoPersonagem;
 
 
+    [Header("Selecção de Personagem")]
+    public Mesh[] skinPersonagem;
+    private MeshFilter meshPersonage;
+    private int idPersonagem; // valor inicial igual a zero
+
+
 	// Use this for initialization
 	void Start () {
         player = FindObjectOfType(typeof(PlayerController)) as PlayerController;
+
+        meshPersonage = player.GetComponentInChildren<MeshFilter>();
+
+
+        idPersonagem = PlayerPrefs.GetInt("IdPersoangemAtual");
+
+        meshPersonage.mesh = skinPersonagem[idPersonagem];
 
         moedasTxt.text = moedas.ToString();
 
@@ -72,6 +85,14 @@ public class ManagerController : MonoBehaviour {
             {
                 hudLoading.SetActive(true);
                 SceneManager.LoadSceneAsync("demo1");
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                SelecionarPersonagem(1);
+            } else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                SelecionarPersonagem(-1);
             }
         }
 	}
@@ -135,6 +156,24 @@ public class ManagerController : MonoBehaviour {
     public void Sair()
     {
         Application.Quit();
+    }
+
+    void SelecionarPersonagem(int i)
+    {
+        idPersonagem += i;
+
+        if(idPersonagem >= skinPersonagem.Length)
+        {
+            idPersonagem = 0;
+        } else if (idPersonagem < 0)
+        {
+            idPersonagem = skinPersonagem.Length - 1;
+        }
+
+        meshPersonage.mesh = skinPersonagem[idPersonagem];
+        PlayerPrefs.SetInt("IdPersoangemAtual", idPersonagem);
+
+        print(idPersonagem);
     }
     #endregion
 }
